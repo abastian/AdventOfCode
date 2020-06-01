d6b6badb6e208d53d2ae17f4589ba98b2e12d7a4
Arief Bastian HB <ariefbastian@gmail.com>
2020-06-01 21:00:47 +0700

    add
    - day-09 (part1 and part2)

---
+++ 2018/day-09/src/main.rs
@@ -0,0 +1,78 @@
+use regex::Regex;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader, Write},
+    num::ParseIntError,
+};
+
+#[derive(Debug)]
+enum Error {
+    IO(io::Error),
+    ParseInt(ParseIntError),
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::IO(err)
+    }
+}
+
+impl From<ParseIntError> for Error {
+    fn from(err: ParseIntError) -> Self {
+        Error::ParseInt(err)
+    }
+}
+
+fn calculate_highest_point(num_player: usize, last_point: u32) -> u64 {
+    let mut points: Vec<u64> = vec![0; num_player];
+    let mut game_arena = Vec::with_capacity(last_point as usize);
+    game_arena.push(0);
+    let mut current = 0usize;
+    for marble in 1..=last_point {
+        if (marble % 23) > 0 {
+            current = ((current + 1) % game_arena.len()) + 1;
+            game_arena.insert(current, marble);
+        } else {
+            let player_idx = marble as usize % num_player;
+            current = (current + game_arena.len() - 7) % game_arena.len();
+            points[player_idx] += (marble + game_arena.remove(current)) as u64;
+        }
+    }
+    *points.iter().max().unwrap()
+}
+
+fn main() -> Result<(), Error> {
+    let file = File::open("input/input.txt")?;
+    let reader = BufReader::new(file);
+
+    if let Some(s) = reader.lines().filter_map(|line| line.ok()).next() {
+        let re = Regex::new(
+            "(?P<players>[0-9]+) players; last marble is worth (?P<points>[0-9]+) points",
+        )
+        .unwrap();
+
+        if let Some(capture) = re.captures(&s) {
+            let num_player: usize = capture["players"].parse()?;
+            let last_point: u32 = capture["points"].parse()?;
+
+            let highest_point = calculate_highest_point(num_player, last_point);
+            writeln!(
+                io::stdout(),
+                "highest point with {} players and last marble worth {} points is {}",
+                num_player,
+                last_point,
+                highest_point
+            )?;
+
+            let highest_point = calculate_highest_point(num_player, 100 * last_point);
+            writeln!(
+                io::stdout(),
+                "highest point with {} players and last marble worth 100 * {} points is {}",
+                num_player,
+                last_point,
+                highest_point
+            )?;
+        }
+    }
+    Ok(())
+}
